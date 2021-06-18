c39f91b647490dbe6249f97e3ebc4dfa62fe5355
LelloPisello <federico.piasenza@gmail.com>
2021-06-18 12:30:26 +0200

    definizioni delle funzioni constexpr spostate nell'header

--- geometry.cc
+++ geometry.cc
@@ -1,16 +1,10 @@
 #include "geometry.hh"
-#include "cmath"
+#include <cmath>
 
 namespace geo
 {
     //vec3:
         //costruttori
-        constexpr vec3::vec3(float x, float y, float z) :
-            x(x), y(y), z(z) {}
-        constexpr vec3::vec3(const vec3& that) :
-            x(that.x), y(that.y), z(that.z) {}
-        constexpr vec3::vec3(float val) :
-            x(val), y(val), z(val) {}
         vec3::vec3(void) = default;
 
         //operator overloading per le parentesi []
@@ -18,59 +12,27 @@ namespace geo
         {
             return &x;
         }
-        //parentesi [] ma costanti
-        constexpr float vec3::at(unsigned index) const
-        {
-            switch(index)
-            {
-                case 0:
-                    return x;
-                case 1:
-                    return y;
-                default:
-                    return z;
-            }
-        }
-        //operator overloading per operazioni matematiche
-        constexpr vec3 vec3::operator + (const vec3& that) const
-        {
-            return vec3(x + that.x, y + that.y, z + that.z);
-        }
-        constexpr vec3 vec3::operator-(const vec3 &that) const
-        {
-            return vec3(x - that.x, y - that.y, z - that.z);
-        }
-        constexpr vec3 vec3::operator*(const vec3 &that) const
-        {
-            return vec3(x * that.x, y * that.y, z * that.z);
-        }
-        constexpr vec3 vec3::operator/(const vec3 &that) const
-        {
-            return vec3(x / that.x, y / that.y, z / that.z);
-        }
+
         //overload per assegnazione speciale
         vec3& vec3::operator+=(const vec3& that)
         {
             return *this = *this + that;
         }
-        vec3 &vec3::operator+=(const vec3 &that)
+        vec3 &vec3::operator-=(const vec3 &that)
         {
             return *this = *this - that;
         }
-        vec3 &vec3::operator+=(const vec3 &that)
+        vec3 &vec3::operator*=(const vec3 &that)
         {
             return *this = *this * that;
         }
-        vec3 &vec3::operator+=(const vec3 &that)
+        vec3 &vec3::operator/=(const vec3 &that)
         {
             return *this = *this / that;
         }
     
     //vec2
         //costruttori
-        constexpr vec2::vec2(float x, float y) : x(x), y(y) {}
-        constexpr vec2::vec2(const vec2& that) : x(that.x), y(that.y) {}
-        constexpr vec2::vec2(float val) : x(val), y(val) {}
         vec2::vec2(void) = default;
 
         //accesso con []
@@ -78,34 +40,7 @@ namespace geo
         {
             return &x;
         }
-        constexpr float vec2::at(unsigned index) const
-        {
-            switch(index)
-            {
-                case 0:
-                    return x;
-                default:
-                    return y;
-            }
-        }
 
-        //overloading operazioni matematiche
-        constexpr vec2 vec2::operator + (const vec2& that) const
-        {
-            return vec2(x + that.x, y + that.y);
-        }
-        constexpr vec2 vec2::operator-(const vec2 &that) const
-        {
-            return vec2(x - that.x, y - that.y);
-        }
-        constexpr vec2 vec2::operator*(const vec2 &that) const
-        {
-            return vec2(x + that.x, y + that.y);
-        }
-        constexpr vec2 vec2::operator/(const vec2 &that) const
-        {
-            return vec2(x + that.x, y + that.y);
-        }
         //assegnazioni speciali
         vec2& vec2::operator+=(const vec2& that)
         {
@@ -143,24 +78,5 @@ namespace geo
         {
             float m = 1. / length(that);
             return that * m;
-        }
-
-        constexpr float dot(const vec3& v1, const vec3& v2)
-        {
-            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-        }
-        constexpr float dot(const vec2& v1, const vec2& v2)
-        {
-            return v1.x * v2.x + v1.y * v2.y;
-        }
-
-        constexpr vec3 cross(const vec3& v1, const vec3& v2)
-        {
-            return vec3
-            {
-                v1.y * v2.z - v1.z * v2.y,
-                v1.z * v2.x - v1.x * v2.z,
-                v1.x * v2.y - v1.y * v2.x
-            };
         }
 }
--- geometry.hh
+++ geometry.hh
@@ -2,6 +2,9 @@
 #define GEOMETRY_h_
 //header guards
 
+
+
+//IMPORTANTE: le funzioni constexpr vengono definite qua
 namespace geo
 {
     struct vec2
@@ -10,23 +13,44 @@ namespace geo
 
         //costruttori
         vec2(void);
-        constexpr vec2(float, float);
-        constexpr vec2(const vec2 &);
-        constexpr vec2(float);
+        constexpr vec2(float x, float y) : x(x), y(y) {}
+        constexpr vec2(const vec2& that) : x(that.x), y(that.y) {}
+        constexpr vec2(float val) : x(val), y(val) {}
 
         ~vec2(void) = default;
 
         //accesso con []
         operator float *(void);
-        constexpr float at(unsigned) const;
+        constexpr float at(unsigned index) const
+        {
+            switch (index)
+            {
+            case 0:
+                return x;
+            default:
+                return y;
+            }
+        }
 
         //operator overloading
         vec2 &operator=(const vec2 &) = default;
 
-        constexpr vec2 operator+(const vec2 &) const;
-        constexpr vec2 operator-(const vec2 &) const;
-        constexpr vec2 operator*(const vec2 &) const;
-        constexpr vec2 operator/(const vec2 &) const;
+        constexpr vec2 operator+(const vec2& that) const
+        {
+            return vec2(x + that.x, y + that.y);
+        }
+        constexpr vec2 operator-(const vec2& that) const
+        {
+            return vec2(x - that.x, y - that.y);
+        }
+        constexpr vec2 operator*(const vec2& that) const
+        {
+            return vec2(x + that.x, y + that.y);
+        }
+        constexpr vec2 operator/(const vec2& that) const
+        {
+            return vec2(x + that.x, y + that.y);
+        }
 
         vec2 &operator+=(const vec2 &);
         vec2 &operator-=(const vec2 &);
@@ -40,22 +64,46 @@ namespace geo
 
         //costruttori
         vec3(void);
-        constexpr vec3(float, float, float);
-        constexpr vec3(const vec3&);
-        constexpr vec3(float);
+        constexpr vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+        constexpr vec3(const vec3& that) : x(that.x), y(that.y), z(that.z) {}
+        constexpr vec3(float val) : x(val), y(val), z(val) {}
 
         ~vec3(void) = default;
 
         //accesso via []
         operator float*(void);
-        constexpr float at(unsigned) const;
+        //[] ma calcolato in compilazione
+        constexpr float at(unsigned index) const
+        {
+            switch (index)
+            {
+            case 0:
+                return x;
+            case 1:
+                return y;
+            default:
+                return z;
+            }
+        }
 
         vec3& operator = (const vec3&) = default;
 
-        constexpr vec3 operator + (const vec3&) const;
-        constexpr vec3 operator - (const vec3&) const;
-        constexpr vec3 operator * (const vec3&) const;
-        constexpr vec3 operator / (const vec3&) const;
+        constexpr vec3 operator + (const vec3& that) const
+        {
+            return vec3(x + that.x, y + that.y, z + that.z);
+        }
+        constexpr vec3 operator - (const vec3& that) const
+        {
+            return vec3(x - that.x, y - that.y, z - that.z);
+        }
+        constexpr vec3 operator * (const vec3& that) const
+        {
+            return vec3(x * that.x, y * that.y, z * that.z);
+        }
+        constexpr vec3 operator / (const vec3& that) const
+        {
+            return vec3(x / that.x, y / that.y, z / that.z);
+        }
 
         vec3& operator += (const vec3 &);
         vec3& operator -= (const vec3 &);
@@ -67,11 +115,27 @@ namespace geo
     float length(const vec2 &);
     vec3 normalize(const vec3 &);
     vec2 normalize(const vec2 &);
+
     //prodotto scalare
-    constexpr float dot(const vec3 &, const vec3 &);
-    constexpr float dot(const vec2 &, const vec2 &);
+    constexpr float dot(const vec3 &a, const vec3 &b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+    constexpr float dot(const vec2 &a, const vec2 &b)
+    {
+        return a.x * b.x + a.y * b.y;
+    }
+
     //prodotto vettoriale
-    constexpr vec3 cross(const vec3 &, const vec3 &);
+    constexpr vec3 cross(const vec3 &a, const vec3 &b)
+    {
+        return vec3
+        {
+            a.y * b.z - a.z * b.y,
+            a.z * b.x - a.x * b.z,
+            a.x * b.y - a.y * b.x
+        };
+    }
 
     struct vertex3D 
     {
