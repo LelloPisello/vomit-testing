8dd2266a6fc349514e1497679d44b2b41b4a969d
Federico Piasenza <70239613+LelloPisello@users.noreply.github.com>
2021-06-25 01:12:30 +0200

    aggiornato entity.hh
    modificato il modo in cui si tiene salvato lo stato di una entità. al posto di avere le informazioni su ogni entità salvate in modo hard-coded, si creerà un array contenente tutte i tipi di entità disponibili al caricamento di una mappa. queste entità avranno alcuni valori condivisi tra ogni tipo, come la mesh, la vita iniziale e la dimensione della bounding box. questo rende il gioco altamente moddabile. da verificare se i bit field possono essere const. sto cercando di tenere minime le dimensioni degli oggetti per rendere il gioco abbastanza leggero. la parte che probabilmente risulterà più rischiosa da ottimizzare sarà il rendering, anche perchè la compilazione delle mappe può anche essere abbastanza lenta.

--- entity.hh
+++ entity.hh
@@ -3,22 +3,27 @@
 //per tenere la posizione e l'angolo delle enità
 #include "geometry.hh"
 
-// struct di base da cui derivano tutte le entità
-struct entity_t
+namespace geo
 {
-    geo::vec3 position;
-    geo::vec3 angle;
-    unsigned short frame_n;
-};
-//struct derivata per entita di tipo mostro 
-struct monster_t:entity_t
-{
-    short health;
-    unsigned char variant;
-};
-
-
-
+    struct entity_info_t //contiene le informazioni condivise per ogni tipo di entità 
+    {
+        const mesh3D model; //[numero varianti]
+        const short starting_health;
+        const vec3 bounding_box_size;
+        //da aggiungere: suoni, varianti di tipo
+    };
+    
+    struct entity_t //contiene le informazioni specifiche di ogni entità
+    {
+        vec3 position;
+        vec3 angle;
+        const unsigned char //non so se funza
+            entity_type : 6; //0 - 64 per il tipo di nemico
+            entity_variant : 2; //0 - 4 per la variante (da implementare)
+        unsigned short frame_n;
+        short health;
+    };
+}
 
 #endif
 
