1b58958a8f75da26516215f5540fe2295020fe23
Federico Piasenza <70239613+LelloPisello@users.noreply.github.com>
2021-06-25 16:03:36 +0200

    creato utils.hh
    aggiunge degli alias di tipo per rendere più riconoscibili le dimensioni dei tipi in bit

---
+++ utils.hh
@@ -0,0 +1,23 @@
+#ifndef UTILS_h_
+#define UTILS_h_
+
+/*
+i: intero con segno + / -
+u: intero solo positivo
+f: virgola mobile con precisione singola
+d: virgola mobile con doppia precisione
+*/
+
+using i8 = char;
+using u8 = unsigned char;
+using i16 = short;
+using u16 = unsigned short;
+using i32 = int;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+using f32 = float;
+using f64 = double;
+using d64 = long double;
+
+#endif
