a3b303fd0c93f1a2726fdcde8c2eca345c60dd3e
Federico Piasenza <70239613+LelloPisello@users.noreply.github.com>
2021-06-25 15:53:25 +0200

    aggiornato  entity.hh
    aggiunta una struttura che fa da lista per le entità di una mappa, rimosso il tipo di entità dalla struttura entità, dato che il tipo si può ricavare da come è piazzata nell' array.

--- entity.hh
+++ entity.hh
@@ -18,10 +18,28 @@ namespace geo
         vec3 position;
         vec3 angle;
         const unsigned char //non so se funza
-            entity_type : 6; //0 - 64 per il tipo di nemico
-            entity_variant : 2; //0 - 4 per la variante (da implementare)
+            //entity_type : 6; rimosso perchè viene dichiarato implicitamente durante il piazzamento nell'array delle entità 
+            entity_variant;// : 2; //0 - 4 per la variante (da implementare)
         unsigned short frame_n;
         short health;
+    };
+    
+    struct entity_list_t
+    {
+        entity_t*** entity_list; //triplo pointer, array sostanzialmente bidimensionale, dato che l'ultimo "passaggio" conterrà un entità sola.
+        const unsigned short num_types; //le dimensioni dell'array principale saranno fisse
+        //costruttori
+        constexpr entity_list_t(void) : entity_list(nullptr) {}
+        explicit entity_list_t(unsigned short num_types);
+        explicit entity_list_t(const char* filename);
+        entity_list_t(const entity_list_t&) = delete;
+        entity_list_t(entity_list&&);
+        ~entity_list_t(void);
+        
+        entity_list_t& from_file(const char* filename); 
+        
+        entity_list_t& operator = (const entity_list_t&) = delete;
+        entity_list_t& operator = (entity_list_t&&);
     };
 }
 
