76419bed6b99003b1f126d977774261f737a4249
Federico Piasenza <70239613+LelloPisello@users.noreply.github.com>
2021-06-26 19:55:47 +0200

    aggiornato map_management.hh
    aggiunta una struttura per i settori BSP e per l'albero BSP
    rimossi i tipi di nemico dalla struttura della mappa.

--- map_management.hh
+++ map_management.hh
@@ -4,6 +4,7 @@
 #include "geometry.hh"
 #include "texture.hh"
 #include "entity.hh"
+#include "utils.hh"
 //caricamento mappe e strutture per contenere i dati sulla mappa
 
 namespace geo
@@ -13,14 +14,22 @@ namespace geo
   pensavo di riempire una di queste strutture ogni volta che entro in una mappa
   contiene la palette della mappa, l'array delle entità, e l'array delle texture
   */
+  
+  struct BSP_sector_t
+  {
+    mesh3D geometry; //geometria del settore
+    plane3D plane; //piano per il bsp
+  };
+  
+  using BSP_tree_t = binary_tree_t
+  <BSP_sector_t>; //albero binario contenente i settori bsp
+  
   struct map_t
   {
     mesh3D* sectors; //settori dopo il BSP (da creare una classe sector)
     unsigned num_sectors; //numero di settori
-    palette_t palette; //palette
+    //palette_t palette; da infilare da qualche altra parte, tipo nella sezione di rendering
     palette_texture_list_t texture_list; //palette
-    entity_info_t* entity_types;//tipi di entità 
-    u16 num_entity_types;
     entity_list_t entity_list; //array contenente le entità, (si potrebbe escludere il numero di tipi da qua dentro)
     map_t(const map_t&);
     map_t(map_t&&);
